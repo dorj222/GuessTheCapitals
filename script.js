44bea4d76f132c64668b0f4b7f59482e0e5c7f0e
dorj222 <dorj222@gmail.com>
2020-07-31 22:31:14 -0400

    updated style.css and scriptjs;

--- script.js
+++ script.js
@@ -1,23 +1,22 @@
 capitalCity = document.getElementById("capitalDisplay");
 options = document.getElementsByClassName("options");
-
-
 generateNewCity = document.getElementById("restart");
+message = document.getElementById("message");
+restart = document.getElementById("restart");
 
-var chosenCountries = [];
-
+let chosenCountries = [];
 let size = 4;
 let correctCity;
 
 reset(size);
 
 function reset(size){
+    correctCity="";
+    chosenCountries = [];
     let indexes = [];
     indexes = generateRandom(size);
     callApi(indexes);
 }
-
-
 
 //Randomly pick the correct country
 function digitRandom(size){
@@ -30,10 +29,10 @@ async function callApi(indexes){
     //With Arrow Function
    const res = await fetch('https://restcountries.eu/rest/v2/all');
    const data =  await res.json();
-    {
+    
    for (let i = 0; i < indexes.length; i++) {
     chosenCountries.push(data[indexes[i]]); 
-   }
+   
   }
   updateDOM(chosenCountries);
 }
@@ -47,14 +46,33 @@ function updateDOM(chosenCountries){
     capitalCity.textContent = correctCapital;
   
     for (let i = 0; i < chosenCountries.length; i++) {  
-        options[i].textContent = chosenCountries[i].name
 
+        options[i].textContent = chosenCountries[i].name
         options[i].addEventListener("click", function(){
+            this.classList.add("clicked");
+        
+
+
             let clickedCountry = this.textContent;
             if(clickedCountry === correctCountry){
-                alert("You are right!");
+                message.textContent = "Correct! 🤗";
+
+                options[pickCountry].classList.add("correct");
+                restart.textContent = "Next"
+                for (let i = 0; i < chosenCountries.length; i++) {
+                    if(i !==pickCountry){options[i].classList.add("incorrect");} 
+                }
+
             } else{
-                alert("Oops!");
+                message.textContent = "Incorrect! 😔";
+                restart.textContent = "Play again?"
+                options[pickCountry].classList.add("correct");
+
+
+                for (let i = 0; i < chosenCountries.length; i++) {
+                 
+                    if(i !==pickCountry){options[i].classList.add("incorrect");} 
+                }      
             }
         })
     }
