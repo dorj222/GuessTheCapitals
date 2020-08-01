a9afc939b9071e0ad0b80e350eeda07b679ad146
dorj222 <dorj222@gmail.com>
2020-08-01 18:43:06 -0400

    updated JS;

--- script.js
+++ script.js
@@ -2,13 +2,19 @@ capitalCity = document.getElementById("capitalDisplay");
 options = document.getElementsByClassName("options");
 message = document.getElementById("message");
 restart = document.getElementById("restart");
+buttonModes = document.getElementsByClassName("mode");
 answerStreak = document.getElementById("answer-streak");
+
 
 let chosenCountries = [];
 let size = 4;
 let correctCity;
 let data = [];
 let correctAnswerStreak = 0;
+
+for (var i = 4; i < options.length; i++) {
+    options[i].style.display = "none";
+}
 
 //initiliazte the game
 init(size);
@@ -17,7 +23,6 @@ function init(size){
   
     indexes = generateRandom(size);
     callApi().then((result)=>{
-    
         for (let i = 0; i < indexes.length; i++) {
             chosenCountries.push(result[indexes[i]]); 
              }
@@ -44,6 +49,7 @@ function updateDOM(chosenCountries){
     pickCountry = digitRandom(size);
     correctCapital = chosenCountries[pickCountry].capital;
     correctCountry = chosenCountries[pickCountry].name;
+    if(correctCapital === "" || correctCountry ===""){reset(size);}
     capitalCity.textContent = correctCapital;
     displayEvertything(chosenCountries);
 }
@@ -79,6 +85,7 @@ function displayCorrectAnswer() {
 
 //show which answers are wrong 
 function displayWrongAnswers(chosenCountries) {
+    correctAnswerStreak = 0;
     for (let i = 0; i < chosenCountries.length; i++) {
         if (i !== pickCountry) { options[i].classList.add("incorrect"); }
     }
@@ -86,16 +93,16 @@ function displayWrongAnswers(chosenCountries) {
 
 function displayDefeat(chosenCountries) {
     message.textContent = "Incorrect! 😔";
-    correctAnswerStreak = 0;
-    answerStreak.textContent = "";
+    //answerStreak.textContent = "";
     restart.textContent = "Play again?";
 }
 
 function displayVictory(chosenCountries) {
     correctAnswerStreak = correctAnswerStreak + 1;
-    // answerStreak.textContent = "Correct answer streak " + correctAnswerStreak +"🔥";
+    //answerStreak.textContent = "Answer streaks " + correctAnswerStreak +"🔥";
     message.textContent = "Correct! 🤗";
     restart.textContent = "Next";
+    
 }
 
 //Randomly choose next four countries
@@ -112,17 +119,20 @@ function generateRandom(size){
 
 //Restart Button
 restart.addEventListener("click", function(){
-    correctCity="";
+    reset(size);
+});
+
+
+function reset(size) {
+    correctCity = "";
     chosenCountries = [];
     let indexes = [];
-    size = 4;
     message.textContent = "";
+    restart.textContent = "Restart";
     resetClassList();
-    correctAnswerStreak = 0;
-    answerStreak.textContent = "";
+    // answerStreak.textContent = "";
     init(size);
-})
-
+}
 
 function resetClassList() {
     for (let i = 0; i < size; i++) {
@@ -130,4 +140,42 @@ function resetClassList() {
         options[i].classList.remove("correct");
         options[i].classList.remove("clicked");
     }
+}
+
+
+for (let i = 0; i < buttonModes.length; i++) {
+    
+    buttonModes[i].addEventListener("click", function(){
+
+       correctAnswerStreak = 0;
+       for (let i = 0; i < buttonModes.length; i++) {
+           buttonModes[i].classList.remove("clicked");
+       }
+
+        if(this.textContent==="Easy"){
+            size = 2;
+            this.classList.add("clicked");
+            for (var i = size; i < options.length; i++) {
+                options[i].style.display = "none"
+            }
+        }
+        else if(this.textContent==="Medium"){
+            size = 4;
+            this.classList.add("clicked");
+            for (var i = size; i < options.length; i++) {
+                options[i].style.display = "none"
+            }
+            for (var i = 2; i < size; i++) {
+                options[i].style.display = "block";
+            }
+        }
+        else if(this.textContent==="Hard"){
+            size = 6;
+            this.classList.add("clicked");
+            for (var i = 4; i < size; i++) {
+                options[i].style.display = "block";
+            }
+        }
+        reset(size);
+    })
 }
